acab3909ad592ce4939b11b607d3c4e434c7631f
dpabon <daniel.epm12@gmail.com>
2015-11-10 15:47:36 -0500

    cabezote

--- order.sh
+++ order.sh
@@ -10,17 +10,19 @@
 
 #Please replace FILE_NAME with the route and file .fasta
 #the script search all lines that have ">". Change the simbol "_" with ";",remove ">" and create an ouput file
-grep ">" DNAdata.fas | sed 's/_/;/g' | sed 's/>//g' > salida.txt
+grep ">" DNAdata.fas | sed 's/_/,/g' | sed 's/>//g' > salida.txt
 #cut the first column in a new file
-cut -f 1 -d ";" salida.txt > id.txt
+cut -f 1 -d "," salida.txt > id.txt
 #cut the second column in a new file
-cut -f 2 -d ";" salida.txt > country.txt
+cut -f 2 -d "," salida.txt > country.txt
 #cut the third column in a new file
-cut -f 3 -d ";" salida.txt > year.txt
+cut -f 3 -d "," salida.txt > year.txt
 #paste the columns, order by year and create an output file
-paste -d ";" country.txt id.txt year.txt | sort > order_country.txt
+paste -d "," country.txt id.txt year.txt | sort > order_country.csv
 #paste the columns, order by country and create an output file
-paste -d ";" year.txt id.txt country.txt | sort > order_year.txt
-#remove the medium o intermediated files
+paste -d "," year.txt id.txt country.txt | sort > order_year.csv
+sed -1i  'probando' order_year.csv 
+#remove unnecessary files
 rm salida.txt id.txt country.txt year.txt
+#philosophical phrase:
 echo "A estudiar para luchar a luchar para estudiar"
