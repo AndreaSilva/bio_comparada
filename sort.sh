fae098f13e293349abb0ef06f41218e19ba29507
dpabon <daniel.epm12@gmail.com>
2015-11-11 22:28:40 -0500

    sort

--- sort.sh
+++ sort.sh
@@ -15,21 +15,15 @@
 
 #Please replace FILE_NAME with the route and file .fasta
 #the script search all lines that have ">". Change the simbol "_" with ";",remove ">" and create an ouput file
-grep ">" DNAdata.fas | sed 's/_/,/g' | sed 's/>//g' > salida.txt
-#cut the first column in a new file
-cut -f 1 -d "," salida.txt > id.txt
-#cut the second column in a new file
-cut -f 2 -d "," salida.txt > country.txt
-#cut the third column in a new file
-cut -f 3 -d "," salida.txt > year.txt
-#paste the columns, sort by year and create an output file
-paste -d "," country.txt id.txt year.txt | sort > sort_country.csv
-#paste the columns, sort by country and create an output file
-paste -d "," year.txt id.txt country.txt | sort > sort_year.csv
+grep ">" FILE_NAME | sed 's/_/,/g' | sed 's/>//g' > salida.txt
+#sort by country and create an output file
+sort -k 2 -t "," salida.txt > sort_country.csv
+#sort by year and create an output file
+sort -k 3 -t "," salida.txt > sort_year.csv
 # create header of files
-sed -l 1 -i  \1i'ISO,ID,YEAR' sort_country.csv
-sed -l 1 -i  \1i'YEAR,ID,ISO' sort_year.csv
+sed -l 1 -i  \1i'ID,ISO,YEAR' sort_country.csv
+sed -l 1 -i  \1i'ID,ISO,YEAR' sort_year.csv
 #remove unnecessary files
-rm salida.txt id.txt country.txt year.txt
+rm salida.txt
 #philosophical phrase:
 echo "A estudiar para luchar a luchar para estudiar"
