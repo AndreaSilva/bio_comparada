2be27953d0f7d8847987eb38b18cb45c0fb952ef
dane <dpabon@openmailbox.org>
2016-01-14 19:20:46 -0500

    Las funciones estadisticas basicas fueron transferidas al archivo functions

--- correlation.R
+++ correlation.R
@@ -17,85 +17,11 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-##########################################
-#  Definición de  funciones de utilidad  #
-##########################################
-#Media
-# Sumatoria de los datos de la variable dividido por la longitud de la variable
-media <- function(x){
-  if (class(x) == "numeric"){
-    media = (sum(x) / length(x))
-    return(media)
-  }else{
-    print("x is not numeric class")
-  }
-}
-# Desviacion estandar
-# 
-des_est <- function(x){
-  if (class(x) == "numeric"){
-    sal = numeric()
-    for(i in 1:length(x)){
-      sal[i] = (x[i] - ((sum(x) / length(x))))^2
-    }
-    des_est = sqrt(sum(sal) / (length(x)-1))
-    return(des_est)
-  }else{
-    print("x is not numeric class")
-  }
-}
 
-# Varianza
-varia <- function(x){
-  if (is.numeric(x) == TRUE){
-    sal <- numeric()
-    for(i in 1:length(x)){
-      sal[i] = (x[i] - ((sum(x) / length(x))))^2
-    }
-    varia = (sum(sal) / (length(x)-1))
-    return(varia)
-  }else{
-    print ("x is not numeric class")
-  }
-}
-
-# covarianza
-cova <- function(x, y){
-  if((class(x) == "numeric" & class(y) == "data.frame")){
-    vec = data.frame()
-    for(i in 1:length(y)){
-      for(c in 1:length(x)){
-        vec[c,i] = ((x[c] - (sum(x)/length(x)))) * ((y[c,i] - (sum(y[ ,i]) / length(y[ ,i]))))
-      }
-    }
-    cova = numeric()
-    for(i in 1:length(vec)){
-      cova[i] = (sum(vec[ ,i]) / (length(vec[ ,i])-1))
-    }
-    names(cova) = colnames(y)
-    return(cova) 
-  }else if((class(x) == "data.frame" & class(y) == "numeric")){
-    vec = data.frame()
-    for(i in 1:length(x)){
-      for(c in 1:length(y)){
-        vec[c,i] = ((y[c] - (sum(y)/length(y)))) * ((x[c,i] - (sum(x[ ,i]) / length(x[ ,i]))))
-      }
-    }
-    cova = numeric()
-    for(i in 1:length(vec)){
-      cova[i] = (sum(vec[ ,i]) / (length(vec[ ,i])-1))
-    }
-    names(cova) = colnames(x)
-    return(cova)
-  }else if ((class(x) == "numeric" & class(y) == "numeric")){
-    vec = numeric()
-    for(i in 1:length(x)){
-      vec[i] = ((x[i] - (sum(x)/length(x)))) * ((y[i] - (sum(y) / length(y))))
-    }
-    cova = sum(vec) / (length(x)-1)
-    return(cova)
-  }
-}
+###############################################
+# Llamando las funciones necesarias           #
+###############################################
+source('functions.R')
 
 ###############################################
 # Preparación de los datos para procesamiento #
