5e8825e254d5a2ecee27fa0fc702d6ab54810d01
eee <eee@dali>
2016-02-15 15:45:57 -0500

    simulations

---
+++ consistencia/simulations.R
@@ -0,0 +1,16 @@
+## Simulations for tree with felsenstein zone
+setwd('~/MEGAsync/bio_comparada/consistencia/')
+source('functions.R')
+
+library(ape)
+library(phyclust)
+library(phytools)
+## Generando un arbol
+gen.felsen.tree()
+
+## Cargando arbol
+arbol <- readChar(con = "felsenstein.tree", nchars =100)
+
+## Generando secuencias
+
+seqgen(opts = "-mGTR -l200 -q", newick.tree = arbol, temp.file = "salida.phy")
