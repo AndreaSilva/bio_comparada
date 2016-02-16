f80b13c9e3c5a3954790cc6929047d516548b057
dane <dpabon@openmailbox.org>
2016-02-15 23:42:57 -0500

    comentarios del codigo

--- consistencia/bin/bash/automatic.sh
+++ consistencia/bin/bash/automatic.sh
@@ -3,12 +3,14 @@
 raiz='/home/eee/MEGAsync/bio_comparada/consistencia/'
 Rscript $raiz'bin/R/gentree.R'
 cd $raiz'data/raw/felsenstein'
+## Modifique por las longitudes de caracteres a analizar
 size=(100 1000 10000)
+## Modifique por el numero de replicas en la simulacion de caracteres
 replicas=$(seq 25)
 for i in ${size[*]};
 do
   for replica in $replicas;
   do
-    seq-gen -mGTR -l$i -or felsenstein.tree > 'salida'$i'_'$replica'.phy'
+    seq-gen -mGTR -l$i -or felsenstein.tree > 'dna'$i'_'$replica'.phy'
   done
 done
