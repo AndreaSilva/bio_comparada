c56306e29d1b0ec271c3e0a2a74d577c3de00505
dane <dpabon@openmailbox.org>
2016-02-15 23:27:54 -0500

    felsenstein automatic data

--- consistencia/bin/bash/automatic.sh
+++ consistencia/bin/bash/automatic.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+## automatización de la creacion de datos a partir de arboles
+raiz='/home/eee/MEGAsync/bio_comparada/consistencia/'
+Rscript $raiz'bin/R/gentree.R'
+cd $raiz'data/raw/felsenstein'
+size=(100 1000 10000)
+replicas=$(seq 25)
+for i in ${size[*]};
+do
+  for replica in $replicas;
+  do
+    seq-gen -mGTR -l$i -or felsenstein.tree > 'salida'$i'_'$replica'.phy'
+  done
+done
