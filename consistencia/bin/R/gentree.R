91f43f9aebedb9a881ab631cc69b17d3e9a0fded
dane <dpabon@openmailbox.org>
2016-02-15 23:54:13 -0500

    lb and sb trees added

--- consistencia/bin/R/gentree.R
+++ consistencia/bin/R/gentree.R
@@ -3,3 +3,13 @@ source('functions.R')
 # felsenstein tree
 setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/felsenstein/')
 gen.felsen.tree()
+#lb
+setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/lb/')
+p <- 0.8
+q <- 0.8
+cat("((t1:",p,",t2:",q,"):",q,",t3:",p,",t4:",q,");", sep = "", file ="lb.tree")
+#sb
+setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/sb/')
+p <- 0.2
+q <- 0.2
+cat("((t1:",p,",t2:",q,"):",q,",t3:",p,",t4:",q,");", sep = "", file ="sb.tree")
