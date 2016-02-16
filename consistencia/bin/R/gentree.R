0bb907d8a2ec9fcedce3feee8728c3191cba0f3d
dane <dpabon@openmailbox.org>
2016-02-16 09:19:51 -0500

    tree.tree correction

--- consistencia/bin/R/gentree.R
+++ consistencia/bin/R/gentree.R
@@ -2,14 +2,14 @@ setwd("~/MEGAsync/bio_comparada/consistencia/bin/R/")
 source('functions.R')
 # felsenstein tree
 setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/felsenstein/')
-gen.felsen.tree()
+gen.felsen.tree(file = "tree.tree")
 #lb
 setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/lb/')
 p <- 0.8
 q <- 0.8
-cat("((t1:",p,",t2:",q,"):",q,",t3:",p,",t4:",q,");", sep = "", file ="lb.tree")
+cat("((t1:",p,",t2:",q,"):",q,",t3:",p,",t4:",q,");", sep = "", file ="tree.tree")
 #sb
 setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/sb/')
 p <- 0.2
 q <- 0.2
-cat("((t1:",p,",t2:",q,"):",q,",t3:",p,",t4:",q,");", sep = "", file ="sb.tree")
+cat("((t1:",p,",t2:",q,"):",q,",t3:",p,",t4:",q,");", sep = "", file ="tree.tree")
