187cfa4e37d1ce79d307f4e8279314a0268f9a68
dane <dpabon@openmailbox.org>
2016-02-15 23:58:03 -0500

    lb and sb dna generated added

--- consistencia/bin/bash/automatic.sh
+++ consistencia/bin/bash/automatic.sh
@@ -7,10 +7,29 @@ cd $raiz'data/raw/felsenstein'
 size=(100 1000 10000)
 ## Modifique por el numero de replicas en la simulacion de caracteres
 replicas=$(seq 25)
+## Felsenstein
 for i in ${size[*]};
 do
   for replica in $replicas;
   do
     seq-gen -mGTR -l$i -or felsenstein.tree > 'dna'$i'_'$replica'.phy'
+  done
+done
+# LB
+cd $raiz'data/raw/lb'
+for i in ${size[*]};
+do
+  for replica in $replicas;
+  do
+    seq-gen -mGTR -l$i -or lb.tree > 'dna'$i'_'$replica'.phy'
+  done
+done
+#SB
+cd $raiz'data/raw/sb'
+for i in ${size[*]};
+do
+  for replica in $replicas;
+  do
+    seq-gen -mGTR -l$i -or sb.tree > 'dna'$i'_'$replica'.phy'
   done
 done
