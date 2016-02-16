3f792d2935b9de17326f9230d87116907ab05cf8
dane <dpabon@openmailbox.org>
2016-02-15 23:32:01 -0500

    make generated

---
+++ consistencia/bin/R/gentree.R
@@ -0,0 +1,5 @@
+setwd("~/MEGAsync/bio_comparada/consistencia/bin/R/")
+source('functions.R')
+# felsenstein tree
+setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/felsenstein/')
+gen.felsen.tree()
