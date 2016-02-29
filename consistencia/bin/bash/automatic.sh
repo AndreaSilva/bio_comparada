2d0cda9590a899d772e87f16b9cd5076dcc620fb
dane <dpabon@openmailbox.org>
2016-02-29 12:52:35 -0500

    seed created

--- consistencia/bin/bash/automatic.sh
+++ consistencia/bin/bash/automatic.sh
@@ -9,32 +9,77 @@ replicas=$(seq 25)
 ## Modifique el modelo a utilizar
 model='GTR'
 ## Semilla para la generación de secuencias
-##seed=228734582635879237485927358962874223452345
+semilla=(-2341135504871372963 289475982475892345 2345234523452452345 -34634563546487958)
+echo $semilla[0]
 ## Cargando directorios
-directorio_raw=($raiz'data/raw/felsenstein/' $raiz'data/raw/lb/' $raiz'data/raw/sb/' $raiz'data/raw/farris/')
+directorio_raw=($raiz'data/raw/farris/' $raiz'data/raw/felsenstein/' $raiz'data/raw/lb/' $raiz'data/raw/sb/')
 ## Creando datos y convirtiendo a fasta
+#: '
 for dic in ${directorio_raw[*]}
 do
   cd $dic
-  for i in ${size[*]}
-  do
-    for replica in $replicas
+  if [ $dic == $raiz'data/raw/felsenstein/' ]
+  then
+    for i in ${size[*]}
     do
-      seq-gen -m$model -l$i tree.tree > 'dna'$i'_'$replica'.phy'
-      readseq -V -all -f='fasta' -o='fasta/dna'$i'_'$replica'.fas' 'dna'$i'_'$replica'.phy'
-      readseq -V -all -f='NEXUS' -o='bayes_input/dna'$i'_'$replica'.nex' 'dna'$i'_'$replica'.phy'
-      cat 'bayes_input/dna'$i'_'$replica'.nex' $raiz'bin/bash/bayes.nex' >> 'bayes_input/dna'$i'_'$replica'.nex'
+      for replica in $replicas
+      do
+        seq-gen -z$((${semilla[0]}+$i+$replica)) -m$model -l$i tree.tree > 'dna'$i'_'$replica'.phy'
+        readseq -V -all -f='fasta' -o='fasta/dna'$i'_'$replica'.fas' 'dna'$i'_'$replica'.phy'
+        readseq -V -all -f='NEXUS' -o='bayes_input/dna'$i'_'$replica'.nex' 'dna'$i'_'$replica'.phy'
+        cat 'bayes_input/dna'$i'_'$replica'.nex' $raiz'bin/bash/bayes.nex' >> 'bayes_input/dna'$i'_'$replica'.nex'
+      done
     done
-  done
+  elif [ $dic == $raiz'data/raw/lb/' ]
+  then
+    for i in ${size[*]}
+    do
+      for replica in $replicas
+      do
+        seq-gen -z$((${semilla[1]}+$i+$replica)) -m$model -l$i tree.tree > 'dna'$i'_'$replica'.phy'
+        readseq -V -all -f='fasta' -o='fasta/dna'$i'_'$replica'.fas' 'dna'$i'_'$replica'.phy'
+        readseq -V -all -f='NEXUS' -o='bayes_input/dna'$i'_'$replica'.nex' 'dna'$i'_'$replica'.phy'
+        cat 'bayes_input/dna'$i'_'$replica'.nex' $raiz'bin/bash/bayes.nex' >> 'bayes_input/dna'$i'_'$replica'.nex'
+      done
+    done
+  elif [ $dic == $raiz'data/raw/sb/' ]
+  then
+    for i in ${size[*]}
+    do
+      for replica in $replicas
+      do
+        seq-gen -z$((${semilla[2]}+$i+$replica)) -m$model -l$i tree.tree > 'dna'$i'_'$replica'.phy'
+        readseq -V -all -f='fasta' -o='fasta/dna'$i'_'$replica'.fas' 'dna'$i'_'$replica'.phy'
+        readseq -V -all -f='NEXUS' -o='bayes_input/dna'$i'_'$replica'.nex' 'dna'$i'_'$replica'.phy'
+        cat 'bayes_input/dna'$i'_'$replica'.nex' $raiz'bin/bash/bayes.nex' >> 'bayes_input/dna'$i'_'$replica'.nex'
+      done
+    done
+  elif [ $dic == $raiz'data/raw/farris/' ]
+  then
+    for i in ${size[*]}
+    do
+      for replica in $replicas
+      do
+        seq-gen -z$((${semilla[3]}+$i+$replica)) -m$model -l$i tree.tree > 'dna'$i'_'$replica'.phy'
+        readseq -V -all -f='fasta' -o='fasta/dna'$i'_'$replica'.fas' 'dna'$i'_'$replica'.phy'
+        readseq -V -all -f='NEXUS' -o='bayes_input/dna'$i'_'$replica'.nex' 'dna'$i'_'$replica'.phy'
+        cat 'bayes_input/dna'$i'_'$replica'.nex' $raiz'bin/bash/bayes.nex' >> 'bayes_input/dna'$i'_'$replica'.nex'
+      done
+    done
+  fi
 done
 
+#'
+: '
 ## Mr Bayes corrida
 for dic in ${directorio_raw[*]};
 do
   cd $dic'bayes_input'
   #rm .nex.*
-  for dato in $( ls );
+  for dato in $( ls )
   do
-    mpirun -np '4' mb $dato
+    #mpirun -np '4' mb $dato
+    echo $dato
   done
 done
+'
