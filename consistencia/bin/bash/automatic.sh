87d3dc3053d7a80da83ec978dfae6d6f3e54b737
dane <dpabon@openmailbox.org>
2016-02-29 01:02:16 -0500

    mr bayes added

--- consistencia/bin/bash/automatic.sh
+++ consistencia/bin/bash/automatic.sh
@@ -9,21 +9,32 @@ replicas=$(seq 25)
 ## Modifique el modelo a utilizar
 model='GTR'
 ## Semilla para la generación de secuencias
-seed=228734582635879237485927358962874223452345
+##seed=228734582635879237485927358962874223452345
 ## Cargando directorios
 directorio_raw=($raiz'data/raw/felsenstein/' $raiz'data/raw/lb/' $raiz'data/raw/sb/' $raiz'data/raw/farris/')
 ## Creando datos y convirtiendo a fasta
-for dic in ${directorio_raw[*]};
+for dic in ${directorio_raw[*]}
 do
   cd $dic
-  for i in ${size[*]};
+  for i in ${size[*]}
   do
-    for replica in $replicas;
+    for replica in $replicas
     do
-      seq-gen -z=$seed -m$model -l$i tree.tree > 'dna'$i'_'$replica'.phy'
+      seq-gen -m$model -l$i tree.tree > 'dna'$i'_'$replica'.phy'
       readseq -V -all -f='fasta' -o='fasta/dna'$i'_'$replica'.fas' 'dna'$i'_'$replica'.phy'
       readseq -V -all -f='NEXUS' -o='bayes_input/dna'$i'_'$replica'.nex' 'dna'$i'_'$replica'.phy'
-      cat -i 'bayes_input/dna'$i'_'$replica'.nex' $raiz'bin/bash/bayes.nex'
+      cat 'bayes_input/dna'$i'_'$replica'.nex' $raiz'bin/bash/bayes.nex' >> 'bayes_input/dna'$i'_'$replica'.nex'
     done
+  done
+done
+
+## Mr Bayes corrida
+for dic in ${directorio_raw[*]};
+do
+  cd $dic'bayes_input'
+  #rm .nex.*
+  for dato in $( ls );
+  do
+    mpirun -np '4' mb $dato
   done
 done
