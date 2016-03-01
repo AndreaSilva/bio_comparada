24ed62adbad008ee7c534c58b577b88cf65887fa
dane <dpabon@openmailbox.org>
2016-03-01 18:30:34 -0500

    directory changed

--- consistencia/bin/R/gentree.R
+++ consistencia/bin/R/gentree.R
@@ -1,18 +1,18 @@
-setwd("~/MEGAsync/bio_comparada/consistencia/bin/R/")
+setwd("~/bio_comparada/consistencia/bin/R/")
 source('functions.R')
 # felsenstein tree
-setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/felsenstein/')
+setwd('~/bio_comparada/consistencia/data/raw/felsenstein/')
 gen.felsen.tree(file = "tree.tree")
 #lb
-setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/lb/')
+setwd('~/bio_comparada/consistencia/data/raw/lb/')
 p <- 0.8
 q <- 0.8
 cat("((t1:",p,",t2:",q,"):",q,",t3:",p,",t4:",q,");", sep = "", file ="tree.tree")
 #sb
-setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/sb/')
+setwd('~/bio_comparada/consistencia/data/raw/sb/')
 p <- 0.2
 q <- 0.2
 cat("((t1:",p,",t2:",q,"):",q,",t3:",p,",t4:",q,");", sep = "", file ="tree.tree")
 # farris
-setwd('~/MEGAsync/bio_comparada/consistencia/data/raw/farris/')
+setwd('~/bio_comparada/consistencia/data/raw/farris/')
 gen.farris.tree(file = "tree.tree")
