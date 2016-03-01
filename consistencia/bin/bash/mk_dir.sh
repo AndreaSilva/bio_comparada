844a8a3c2e87616a2acef79ca1be59a5e48788c4
dane <dpabon@openmailbox.org>
2016-03-01 15:08:00 -0500

    estructura de carpetas added

---
+++ consistencia/bin/bash/mk_dir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## creación de directorios de datos necesarios para correr los archivos
+cd ../..
+raiz=$pwd
+mkdir data
+cd data
+mkdir raw processed
+mkdir raw/farris/ raw/felsenstein/ raw/lb/ raw/sb/
+mkdir raw/farris/fasta raw/felsenstein/fasta raw/lb/fasta raw/sb/fasta
+mkdir raw/farris/bayes_input raw/felsenstein/bayes_input raw/lb/bayes_input raw/sb/bayes_input
+mkdir processed/farris/ processed/felsenstein/ processed/lb/ processed/sb/
+mkdir processed/farris/bayes_output processed/felsenstein/bayes_output processed/lb/bayes_output processed/sb/bayes_output
